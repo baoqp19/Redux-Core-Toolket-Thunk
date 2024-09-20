0530c55eb77cf0f92e0d41f8c0172683f5c30f21
= <=>
2024-09-20 15:03:43 +0700

    Redux-Toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from "./App.jsx"
+import React from "react";
+import ReactDOM from "react-dom/client"; // Sử dụng 'react-dom/client' thay vì 'react-dom'
+import App from "./App.jsx";
+import store from "./redux/store.jsx";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root")); // Tạo root element
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
